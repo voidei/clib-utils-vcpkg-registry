c4ab3dcbe86ad611244a81525d701c4dfcd942c6
Dawn Walker <nosirrah.reklaw@gmail.com>
2023-01-01 22:39:01 +1100

    linting and such

--- ports/mergemapper/portfile.cmake
+++ ports/mergemapper/portfile.cmake
@@ -1,15 +1,22 @@
 # header-only library
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO alandtse/MergeMapper
-    REF 2630b22661a23d04b2298bcd89e2dc24bfe7bfe7
-    SHA512 050752ab2e6e83dec76b51ef7f7925699f3829982d7d9cbd003c21c2aff4eb2c9c6b8bc6b4ee4ffd78a23812b2e1d0b7aff55594d91587c29d16edecdcdda342
-    HEAD_REF main
-)
+    OUT_SOURCE_PATH
+    SOURCE_PATH
+    REPO
+    alandtse/MergeMapper
+    REF
+    2630b22661a23d04b2298bcd89e2dc24bfe7bfe7
+    SHA512
+    050752ab2e6e83dec76b51ef7f7925699f3829982d7d9cbd003c21c2aff4eb2c9c6b8bc6b4ee4ffd78a23812b2e1d0b7aff55594d91587c29d16edecdcdda342
+    HEAD_REF
+    main)
 
 # Install codes
 set(MERGEMAPPER_SOURCE ${SOURCE_PATH}/src/MergeMapperPluginAPI.cpp ${SOURCE_PATH}/include/MergeMapperPluginAPI.h)
 
 file(INSTALL ${MERGEMAPPER_SOURCE} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(
+    INSTALL ${SOURCE_PATH}/LICENSE
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+    RENAME copyright)
--- ports/simpleini-po3/portfile.cmake
+++ ports/simpleini-po3/portfile.cmake
@@ -1,21 +1,23 @@
 # header-only library
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO powerof3/simpleini
-    REF 00cf4ad2a58ae43b52f0cb4004e6f1ee4752d900
-    SHA512 c9a8527e67f1273c52d61e3daf0fdee223196c4d4cd2b6a04ba58b0c297c0d36420c7357a7a73a56d29c463a6b28559f412314fc389909465df4963090badc42
-    HEAD_REF master
-)
+    OUT_SOURCE_PATH
+    SOURCE_PATH
+    REPO
+    powerof3/simpleini
+    REF
+    00cf4ad2a58ae43b52f0cb4004e6f1ee4752d900
+    SHA512
+    c9a8527e67f1273c52d61e3daf0fdee223196c4d4cd2b6a04ba58b0c297c0d36420c7357a7a73a56d29c463a6b28559f412314fc389909465df4963090badc42
+    HEAD_REF
+    master)
 
 # Install codes
-set(SIMPLEINI_PO3_SOURCE
-    ${SOURCE_PATH}/SimpleIni.h
-    ${SOURCE_PATH}/ConvertUTF.h
-    ${SOURCE_PATH}/ConvertUTF.c
-)
+set(SIMPLEINI_PO3_SOURCE ${SOURCE_PATH}/SimpleIni.h ${SOURCE_PATH}/ConvertUTF.h ${SOURCE_PATH}/ConvertUTF.c)
 
 file(INSTALL ${SIMPLEINI_PO3_SOURCE} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
-# Install sample
-# file(INSTALL ${SOURCE_PATH}/snippets.cpp DESTINATION ${CURRENT_PACKAGES_DIR}/share/sample)
-file(INSTALL ${SOURCE_PATH}/LICENCE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+# Install sample file(INSTALL ${SOURCE_PATH}/snippets.cpp DESTINATION ${CURRENT_PACKAGES_DIR}/share/sample)
+file(
+    INSTALL ${SOURCE_PATH}/LICENCE.txt
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+    RENAME copyright)
